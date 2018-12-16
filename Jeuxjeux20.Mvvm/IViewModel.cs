f3164a4de3259ffcc1ab291a5f01a27ba5da6ebc
jeuxjeux20 Y <jeuxjeux20@outlook.fr>
2018-12-16 22:36:12 +0100

    did the thing

---
+++ Jeuxjeux20.Mvvm/IViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace Jeuxjeux20.Mvvm
+{
+    public interface IViewModel<out T> : INotifyPropertyChanged
+    {
+        T Model { get; }
+    }
+}
---
+++ Jeuxjeux20.Mvvm/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿namespace Jeuxjeux20.Mvvm
+{
+    public class ViewModelBase<T> : PropertyChangedObject, IViewModel<T> where T : class, new()
+    {
+        public ViewModelBase(T model = null)
+        {
+            if (model == null)
+            {
+                // ReSharper disable once VirtualMemberCallInConstructor because this is intended
+                model = BuildDefaultModel();
+            }
+            Model = model;
+        }
+        public ViewModelBase() : this(null) { }
+        public T Model { get; protected set; }        
+        public override string ToString()
+        {
+            var modelToString = Model.ToString();
+            return modelToString == Model.GetType().ToString() ? base.ToString() : modelToString;
+        }
+
+        protected virtual T BuildDefaultModel()
+        {
+            return new T();
+        }
+    }
+}
